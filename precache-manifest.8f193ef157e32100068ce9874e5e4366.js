23de067a29cf219eb17e148d787979e7a21b23a6
M4rcoRamirez <m4rco.ramirez@gmail.com>
2020-06-03 23:12:42 -0500

    Updates

---
+++ precache-manifest.8f193ef157e32100068ce9874e5e4366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcf1120856dc778729ba5dde0cc4f8f",
+    "url": "/prueba/index.html"
+  },
+  {
+    "revision": "51184dca2409b0cfa446",
+    "url": "/prueba/static/css/main.f7eba527.chunk.css"
+  },
+  {
+    "revision": "c6f50f8738dfb9adbb21",
+    "url": "/prueba/static/js/2.fde2b214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba/static/js/2.fde2b214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51184dca2409b0cfa446",
+    "url": "/prueba/static/js/main.b884a44d.chunk.js"
+  },
+  {
+    "revision": "14ed90595446e379682b",
+    "url": "/prueba/static/js/runtime-main.e8b59b7b.js"
+  }
+]);
